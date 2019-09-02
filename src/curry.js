bb360b47a64babafa167808e97b1911340d84f3f
vilscrolls <vilscrolls@gmail.com>
2019-09-02 18:45:15 +0800

    Add function "curry"

---
+++ src/curry.js
@@ -0,0 +1,17 @@
+/**
+ * Returns a curried version of the provided function.
+ *
+ * @sig ((a, b, ...) -> c) -> a -> b -> ... -> c
+ * @param {Function} func The function to curry
+ * @param {...Args=} parameters Parameters to be partially applied
+ * @return {Function} A curried function
+ * @example
+ *
+ * import { curry } from 'funale'
+ *
+ * const sum = (a, b) => a + b
+ * const inc = curry(sum, 1)
+ * inc(2) // 3
+ */
+const curry = (f, ...args) => (f.length ? ( args.length >= f.length ? f(...args) : (...rest) => curry(f, ...args, ...rest) ) : f)
+export default curry
