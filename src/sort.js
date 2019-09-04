0297c6c14cbc944de44751aa4895c6656b270d0b
Tan <vilscrolls@gmail.com>
2019-09-04 18:39:49 +0800

    Add missing example for "sort" (#8)

--- src/sort.js
+++ src/sort.js
@@ -8,6 +8,11 @@ import _curry2 from './internal/_curry2'
  * @param {Function} comparator A sorting function
  * @param {Array} list The list to sort
  * @return {Array} A new sorted array
+ * @example
+ *
+ * import { sort } from 'funale'
+ *
+ * sort((a, b) => a - b, [2, 1, 4, 1, 3, 5]) // [1, 1, 2, 3, 4, 5]
  */
 const sort = _curry2((comparator, list) => Array.prototype.slice.call(list, 0).sort(comparator))
 export default sort
