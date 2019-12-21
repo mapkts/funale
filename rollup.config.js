58d01b193a5e7462d96eb4b39ba288f2559667f1
mapkts <vilscrolls@gmail.com>
2019-12-21 17:38:02 +0800

    refactor build process

--- rollup.config.js
+++ rollup.config.js
@@ -4,23 +4,30 @@ const uglify = require('rollup-plugin-uglify').uglify
 
 const config = {
   input: 'src/index.js',
-  output: {
-    file: 'dist/funale.js',
+  output: [
+    {
+    file: 'dist/funale.umd.js',
     format: 'umd',
     name: 'F',
     exports: 'named',
   },
+],
   plugins: [
     resolve(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
+    babel({ presets: [['@babel/preset-env', { targets: { ie: '11' } }]] }),
   ],
 }
 
 if (process.env.NODE_ENV === 'production') {
   config.plugins.push(
-    uglify()
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+      },
+      warnings: false,
+    })
   )
 }
 
